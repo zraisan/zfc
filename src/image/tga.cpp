e7d04b4eb7ea2af691ce6c50bfd5c191f0761942
Zain Raisan <zain.raisan@dmt.gov.ae>
2026-03-17 14:21:46 +0400

    feat: Added TGA ingestion and generation

---
+++ src/image/tga.cpp
@@ -0,0 +1,40 @@
+#include "tga.hpp"
+#include <vector>
+
+tga::FileHeader tga::readHeader(std::vector<unsigned char> &binary)
+{
+    tga::FileHeader header;
+    header.fileIdLength = binary[0];
+    header.width = binary[12] | (binary[13] << 8);
+    header.height = binary[14] | (binary[15] << 8);
+    header.bitsPerPixel = binary[16];
+    header.rightToLeft = (binary[17] >> 4) & 1;
+    header.topToBottom = (binary[17] >> 5) & 1;
+    header.offset = 18 + header.fileIdLength;
+    return header;
+}
+
+std::vector<unsigned char>
+tga::readImageData(std::vector<unsigned char> &imageBinary)
+{
+    tga::FileHeader header = tga::readHeader(imageBinary);
+    int y;
+    bool condition;
+    int increment;
+    std::vector<unsigned char> imageData;
+    for (int y = 0; y < header.height; y++)
+    {
+        int row = header.topToBottom ? y : header.height - 1 - y;
+        for (int x = 0; x < header.width * 3; x += 3)
+        {
+            int col = header.rightToLeft ? x : header.width * 3 - 1 - x;
+            int r = col + 2;
+            int g = col + 1;
+            int b = col;
+            imageData.push_back(imageBinary[header.offset + r]);
+            imageData.push_back(imageBinary[header.offset + g]);
+            imageData.push_back(imageBinary[header.offset + b]);
+        }
+    }
+    return imageData;
+}
---
+++ src/image/tga.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <fstream>
+
+namespace tga
+{
+    struct FileHeader
+    {
+        uint8_t fileIdLength;
+        uint32_t offset;
+        uint16_t width;
+        uint16_t height;
+        bool rightToLeft;
+        bool topToBottom;
+        uint8_t bitsPerPixel;
+    };
+
+    FileHeader readHeader(std::vector<unsigned char> &binary);
+    std::vector<unsigned char>
+    readImageData(std::vector<unsigned char> &imageBinary);
+
+    template <typename FH>
+    void generate(FH &imageHeader, std::vector<unsigned char> &imageBinary,
+                  std::string outputPath)
+    {
+        std::ofstream output(outputPath, std::ios::binary);
+        uint8_t zero8 = 0;
+        uint16_t zero16 = 0;
+        int zero = 0;
+        u_int8_t imageDescriptor = 32;
+
+        output.write(reinterpret_cast<const char *> & zero8, 1);
+        output.write(reinterpret_cast<const char *> & zero8, 1);
+        output.write(reinterpret_cast<const char *> & zero8, 1);
+        output.write(reinterpret_cast<const char *> & zero, 5);
+        output.write(reinterpret_cast<const char *> & zero16, 2);
+        output.write(reinterpret_cast<const char *> & zero16, 2);
+        output.write(reinterpret_cast<const char *> & imageHeader.width, 2);
+        output.write(reinterpret_cast<const char *> & imageHeader.height, 2);
+        output.write(reinterpret_cast<const char *> & imageHeader.bitsPerPixel, 1);
+        output.write(reinterpret_cast<const char *> & imageDescriptor, 1);
+        }
+
+} // namespace tga
