12a64584e97dc261d7c6d60512e93951e587dd92
Zain Raisan <zain.raisan@dmt.gov.ae>
2026-04-02 14:11:25 +0400

    feat: stage 1 huffman coding

--- src/utils/deflate.cpp
+++ src/utils/deflate.cpp
@@ -4,19 +4,19 @@
 #include <algorithm>
 #include <string>
 
-std::vector<deflate::LZ77> deflate::compress(std::vector<unsigned char> &binary, int sliding_window_size, int lookahead_buffer)
+std::vector<deflate::LZ77> deflate::compress(std::vector<unsigned char> &binary, int window_size)
 {
     int idx = 0;
     std::vector<deflate::LZ77> compressed;
     while (idx < binary.size())
     {
-        int sw_start = std::max(0, idx - sliding_window_size);
+        int sw_start = std::max(0, idx - window_size);
         int best_offset = 0;
         int best_length = 0;
         for (int i = sw_start; i < idx; i++)
         {
             int length = 0;
-            int max_len = std::min(lookahead_buffer, (int)binary.size() - idx);
+            int max_len = std::min(MAX_MATCH_LENGTH, (int)binary.size() - idx);
             while (length < max_len && binary[i + length] == binary[idx + length])
             {
                 length++;
@@ -30,7 +30,7 @@ std::vector<deflate::LZ77> deflate::compress(std::vector<unsigned char> &binary,
         if (best_length > 0)
         {
             int next_idx = idx + best_length;
-            compressed.push_back({best_offset, best_length, 0});
+            compressed.push_back({best_offset, best_length, binary[next_idx]});
             idx += best_length + 1;
         }
         else
@@ -58,4 +58,76 @@ std::vector<unsigned char> deflate::decompress(std::vector<deflate::LZ77> &compr
         }
     }
     return binary;
+}
+
+std::vector<unsigned char> deflate(std::vector<unsigned char> &binary)
+{
+    int window_size = deflate::WINDOW_SIZE;
+    if (binary.size() <= 16384)
+        window_size = std::pow(2, std::floor(std::sqrt(binary.size())));
+    std::vector<deflate::LZ77> lz_compressed = deflate::compress(binary, window_size);
+    std::vector<unsigned char> literal_stream;
+    std::vector<unsigned char> distance_stream;
+    for (int i = 0; i < lz_compressed.size(); i++)
+    {
+        if (lz_compressed[i].L > 2) // In general it is cheaper to group length of 1 and 2 with 0
+        {
+            literal_stream.push_back(lz_compressed[i].L);
+            literal_stream.push_back(lz_compressed[i].C);
+
+            distance_stream.push_back(lz_compressed[i].B);
+        }
+        else
+        {
+            literal_stream.push_back(lz_compressed[i].C);
+
+            distance_stream.push_back(lz_compressed[i].B);
+        }
+    }
+
+    int literal_freq[286];  // Standard size: (0-255) Byte - (256) End of Block - (257-285) Length Codes (Grouped)
+    int distance_freq[258]; // Standard size (Grouped)
+
+    for (auto &token : lz_compressed)
+    {
+        if (token.L > 0)
+        {
+            literal_freq[token.L + 254]++;
+        }
+        else
+        {
+            literal_freq[token.C]++;
+        }
+    }
+}
+
+int length_to_code(int length)
+{
+    if (length <= 10)
+        return 254 + length; // 257-264
+    if (length <= 18)
+        return 265 + (length - 11) / 2;
+    if (length <= 34)
+        return 269 + (length - 19) / 4;
+    if (length <= 66)
+        return 273 + (length - 35) / 8;
+    if (length <= 130)
+        return 277 + (length - 67) / 16;
+    if (length <= 257)
+        return 281 + (length - 131) / 32;
+    return 285; // 258
+}
+
+int distance_to_code(int dist)
+{
+    if (dist <= 4)
+        return dist - 1; // 0-3
+    int code = 2;
+    int d = dist - 1;
+    while (d >= 2)
+    {
+        d >>= 1;
+        code++;
+    }
+    return code;
 }
--- src/utils/deflate.hpp
+++ src/utils/deflate.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <cstdint>
 
 namespace deflate
 {
@@ -10,6 +11,22 @@ namespace deflate
         int L; // How many bytes to copy from that match
         int C; // The next literal byte that didn't fit into the match
     };
-    std::vector<LZ77> compress(std::vector<unsigned char> &binary, int sliding_window_size, int lookahead_buffer);
+    struct HuffmanNode
+    {
+        uint8_t value;
+        int freq;
+        HuffmanNode *left;
+        HuffmanNode *right;
+        HuffmanNode(uint8_t v, int freq, HuffmanNode *left = nullptr, HuffmanNode *right = nullptr)
+            : value(v), freq(freq), left(left), right(right)
+        {
+        }
+    };
+    const int WINDOW_SIZE = 32768;
+    const int MAX_MATCH_LENGTH = 258;
+
+    std::vector<LZ77> compress(std::vector<unsigned char> &binary, int window_size = WINDOW_SIZE);
     std::vector<unsigned char> decompress(std::vector<LZ77> &compressed_binary);
+    std::vector<unsigned char> deflate(std::vector<unsigned char> &binary);
+    std::vector<unsigned char> inflate(std::vector<unsigned char> &binary);
 }
