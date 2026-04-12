3f1282ffe9bbb6841bf0c20cf9b7aa41fd9c2a04
mzkux <zain.kml.raisan@gmail.com>
2026-04-12 14:09:52 +0400

    feat: Implemented inflate function

--- src/utils/deflate.hpp
+++ src/utils/deflate.hpp
@@ -22,6 +22,17 @@ namespace deflate
         {
         }
     };
+    struct RleSym
+    {
+        int sym;   // code-length alphabet symbol (0-18)
+        int extra; // value for the extra bits (0 if sym < 16)
+    };
+    struct BitReader
+    {
+        const std::vector<unsigned char> *data;
+        size_t byte_idx;
+        int bit_pos;
+    };
     const int WINDOW_SIZE = 32768;
     const int MAX_MATCH_LENGTH = 258;
 
@@ -29,4 +40,6 @@ namespace deflate
     std::vector<unsigned char> decompress(std::vector<LZ77> &compressed_binary);
     std::vector<unsigned char> deflate(std::vector<unsigned char> &binary);
     std::vector<unsigned char> inflate(std::vector<unsigned char> &binary);
+    std::vector<unsigned char> make_zlib(const std::vector<unsigned char> &original,
+                                         const std::vector<unsigned char> &deflated);
 }
